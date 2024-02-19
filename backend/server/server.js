f93993e6aba3062b8171bc4d2b882671e1dee0c2
David Gann <davycrocket2003@gmail.com>
2024-02-19 14:28:00 -0700

    Initial Commit

---
+++ backend/server/server.js
@@ -0,0 +1,48 @@
+// probably going to need express, morgan, express-session, ViteExpress
+import express from "express"
+import morgan from "morgan"
+import session from "express-session"
+import ViteExpress from "vite-express"
+import http from "http"
+import { Server } from "socket.io"
+
+
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+
+app.use(morgan("dev"))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(
+  session({
+    secret: "heeeeeey",
+    saveUninitialized: false,
+    resave: false,
+  })
+)
+
+
+
+//Routes
+app.get('/', (req, res) => {
+    res.sendFile('/Users/davyc/devMountain/practiceServer/src/index.html')
+})
+
+io.on('connection', (socket) => {
+    console.log('user connected')
+    socket.on('disconnect', () => {
+      console.log('user disconnected')
+    })
+  })
+
+
+
+
+
+
+
+// Run the server
+server.listen( 8181, () => console.log('Now listening on http://localhost:8181'))
